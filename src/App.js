89e38ce38336da25b257f8d625d1b765279cea30
HSR <hectorsilvarobles@gmail.com>
2019-11-14 10:52:37 -0700

    components made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header.js'
+import Hero from './Components/Hero/Hero.js'
+import Intro from './Components/Intro/Intro.js'
+import Services from './Components/Services/Services.js'
+import Work from './Components/Work/Work.js'
+import Testimonials from './Components/Testimonials/Testimonials.js'
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Hero />
+      <Intro />
+      <Services />
+      <Work />
     </div>
   );
 }
---
+++ src/Components/Work/Work.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import './work.scss'
+
+export class Work extends Component {
+    render(){
+        return (
+            <div>
+                <h1>
+                    work
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default Work
